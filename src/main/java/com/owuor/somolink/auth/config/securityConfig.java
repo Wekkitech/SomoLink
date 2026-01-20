58b3a515ea04ef41ff051eba876170885d2105f9
Chrispine Owuor <104433015+ChrisOwuor@users.noreply.github.com>
2026-01-21 02:09:50 +0300

    update:updated the workflow file

--- src/main/java/com/owuor/somolink/auth/config/securityConfig.java
+++ src/main/java/com/owuor/somolink/auth/config/securityConfig.java
@@ -77,7 +77,7 @@ public class securityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://40.127.14.27","*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
