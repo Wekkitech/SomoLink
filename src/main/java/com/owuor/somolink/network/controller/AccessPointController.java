3933c437bf0cb6b6f31b18c127a0dcb22d8de923
Chrispine Owuor <104433015+ChrisOwuor@users.noreply.github.com>
2026-01-21 00:56:45 +0300

    update:updated the workflow file

--- src/main/java/com/owuor/somolink/network/controller/AccessPointController.java
+++ src/main/java/com/owuor/somolink/network/controller/AccessPointController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/aps")
 public class AccessPointController {
+    //homw
 
     private final AccessPointService apService;
 
